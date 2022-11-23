356a11fc10365b96554711bc917dc1d501b34403
Pete Bell <pdtbell@gamil.com>
2022-11-24 03:21:22 +1100

    First App

---
+++ first-app/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-base>
+
+<x-slot name="name">
+    Petey
+</x-slot>
+
+<x-slot name="content">
+    <h1 class="text-4xl font-bold">Home</h1>
+    <p class="mt-4 text-lg">Lorem ipsum, blah blah blah</p>
+</x-slot>
+
+</x-base>
