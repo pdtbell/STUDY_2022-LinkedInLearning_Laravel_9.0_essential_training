c08223625a2966a64729498cea4f933ca7d07e99
Pete Bell <pdtbell@gamil.com>
2022-12-01 15:24:01 +1100

    moving on to litenotes

---
+++ first-app/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class WelcomeController extends Controller
+{
+    public function index() {
+        // 1. Using raw SQL
+        // $users = DB::select('select * from users');
+        // dd($users);
+
+        // 2. Query Builder
+        // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($users);
+
+        // 3. Eloquent ORM
+        // $students = Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($students);
+        // foreach($students as $student) {
+        //     echo $student->name."<br/>";
+ 
+        // }
+        $student = new Student();
+        $student->name = "New";
+        $student->email = "email@gmail.com";
+        $student->save();
+    }
+}
